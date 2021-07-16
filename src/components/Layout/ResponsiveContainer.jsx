ef5af6965f6a3439a6f583c736ad8a51194fc8d2
Gabriel Silva <j.gabriel.888@gmail.com>
2021-07-16 11:26:39 -0400

    format timestamp to readable date

--- src/components/Layout/ResponsiveContainer.jsx
+++ src/components/Layout/ResponsiveContainer.jsx
@@ -2,22 +2,42 @@ import Card from '../UI/Card';
 import CommonButtonGroup from './CommonButtonGroup';
 import { sampleData } from '../../data/sampleData';
 
+const monthName = [
+	'Jan',
+	'Feb',
+	'Mar',
+	'Apr',
+	'May',
+	'Jun',
+	'Jul',
+	'Aug',
+	'Sep',
+	'Oct',
+	'Nov',
+	'Dez',
+];
+
 const ResposiveContainer = () => {
 	let eachCard = sampleData.map((data) => {
+		let formattedDate = new Intl.DateTimeFormat('en-US', {
+			year: 'numeric',
+			month: 'short',
+			day: '2-digit',
+			hour: '2-digit',
+			minute: '2-digit',
+		}).format(data.dateUpdated);
 		return (
-			<div className="container">
-				<Card key={data.id}>
-					<h1>{data.name}</h1>
-					<h3>{data.originType} Origin</h3>
-					<p>{data.intents}</p>
-					<p>{data.dateUpdated}</p>
-					<CommonButtonGroup />
-				</Card>
-			</div>
+			<Card key={data.id}>
+				<h1>{data.name}</h1>
+				<h3>{data.originType} Origin</h3>
+				<p>{data.intents}</p>
+				<p>{formattedDate}</p>
+				<CommonButtonGroup />
+			</Card>
 		);
 	});
 
-	return <div>{eachCard}</div>;
+	return <div className="container">{eachCard}</div>;
 };
 
 export default ResposiveContainer;
