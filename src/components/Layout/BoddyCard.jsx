1db20111c535f99b8e140db3e103f6a1ee666ca0
Gabriel Silva <j.gabriel.888@gmail.com>
2021-07-16 15:08:38 -0400

    style card header and title

--- src/components/Layout/BoddyCard.jsx
+++ src/components/Layout/BoddyCard.jsx
@@ -1,8 +1,10 @@
+import '../../styles/BoddyCard.scss';
+
 const BoddyCard = (props) => {
 	return (
-		<div>
-			<h3>{props.skillName}</h3>
-			<span>{props.intents}</span>
+		<div className="boddyCard">
+			<h3 className="boddyTitlte">{props.skillName}</h3>
+			<p className="subText">{props.intents}</p>
 		</div>
 	);
 };
